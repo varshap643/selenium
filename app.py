bfd8a7ca7442fb2a3004a0c0ef312cafb61c0f1e
varshap643 <pendyalavarsha847@gmail.com>
2025-10-23 15:18:46 +0530

    last commit

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def submit():
     return render_template('greeting.html', name=username)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port= 5000, debug = True)
+    app.run(host='127.0.0.1', port= 5000, debug = True)
